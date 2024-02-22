06f5b868505d231440a03d0e939785a157eed4d5
Chaos <khavtpc04879@fpt.edu.vn>
2024-02-22 13:55:02 +0700

    Revert "Revert "FE Admin""
    This reverts commit af301efc23738497b8e5e16178e6c57b940f9838.

---
+++ src/main/java/com/motel/controller/AdminProfileController.java
@@ -0,0 +1,12 @@
+package com.motel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminProfileController {
+    @GetMapping("/admin/profile")
+    public String showProfile(){
+        return "admin/profile/app-profile";
+    }
+}

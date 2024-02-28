84f4aceb610a4cf66d362f5bbb465e2422453e39
Chaos <khavtpc04879@fpt.edu.vn>
2024-02-29 00:41:56 +0700

    fix

--- src/main/java/com/motel/controller/RoomController.java
+++ src/main/java/com/motel/controller/RoomController.java
@@ -1,7 +1,9 @@
 package com.motel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +13,8 @@ import com.motel.service.AccountService;
 @Controller
 public class RoomController {
     @GetMapping("/room")
-    public String getMethodName() {
+    public String getMethodName(Authentication authentication, Model model) {
+        model.addAttribute("accountId", authentication.getName());
         return "home/room";
     }
     

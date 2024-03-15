31b57fa4dd47aea9df84c9fffc476dccf0a1f299
Chaos <khavtpc04879@fpt.edu.vn>
2024-03-15 08:34:11 +0700

    fix

--- src/main/java/com/motel/repository/ElectricityCashRepository.java
+++ src/main/java/com/motel/repository/ElectricityCashRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 import com.motel.entity.ElectricityCash;
 
-
 public interface ElectricityCashRepository extends JpaRepository<ElectricityCash, Integer>{
     @Query("SELECT rc FROM ElectricityCash rc WHERE rc.motelRoom.motelRoomId = :motelRoomId")
     ElectricityCash findByMotelIdOfElectricityCash(@Param("motelRoomId") Integer motelRoomId);
